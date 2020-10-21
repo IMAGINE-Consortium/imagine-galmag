ae578831952669c4234eda527bf0e54d738c9a22
Luiz Felippe S. Rodrigues <luizfelippesr@gmail.com>
2020-10-21 17:14:19 +0200

    Adjusts GalMag Fields to current IMAGINE conventions.

--- imagine_galmag/field.py
+++ imagine_galmag/field.py
@@ -17,39 +17,39 @@ class GalMagMagneticFieldBase(MagneticField):
     """
     def __init__(self, grid, *, parameters=dict(), ensemble_size=None,
                  ensemble_seeds=None, dependencies={}, keep_galmag_field=False):
-        
+
         self.keep_galmag_field = keep_galmag_field
         # GalMag uses standard units
         box_dimensionless = grid.box.to_value(u.kpc)
-        
+
         # Prepares a GalMag field generator with grid details
         self.galmag_gen = self.galmag_generator_class(box=box_dimensionless,
                                                       resolution=grid.resolution,
                                                       grid_type=grid.grid_type)
         # Caches galmag object
         self.galmag = None
-        # Any functions (e.g. profiles) and switches (e.g. 'disk_field_decay') 
+        # Any functions (e.g. profiles) and switches (e.g. 'disk_field_decay')
         # are stored (by subclasses) in the following attribute
         self._field_options = {}
-        
+
         super().__init__(grid=grid, parameters=parameters, ensemble_size=ensemble_size,
                          ensemble_seeds=ensemble_seeds, dependencies=dependencies)
-    
+
     def compute_field(self, seed):
-        
+
         if self.galmag is not None:
             galmag_B = self.galmag
         else:
             parameters = self.parameters.copy()
             parameters.update(self._field_options)
             galmag_B = self.galmag_gen.get_B_field(**parameters)
-        
+
         B_array = np.empty(self.data_shape)
         # and saves the pre-computed components
         B_array[:,:,:,0] = galmag_B.x
         B_array[:,:,:,1] = galmag_B.y
         B_array[:,:,:,2] = galmag_B.z
-        
+
         if self.keep_galmag_field:
             self.galmag = galmag_B
 
@@ -61,6 +61,12 @@ class GalMagDiskField(GalMagMagneticFieldBase):
     An IMAGINE field constructed using GalMag's disk magnetic field
     """
     NAME = 'galmag_disk_magnetic_field'
+    # The following is updated dynamically with the normalization of
+    # different eigenmodes
+    PARAMETER_NAMES = ['disk_modes_normalization', 'disk_height',
+                       'disk_radius', 'disk_turbulent_induction',
+                       'disk_dynamo_number', 'disk_regularization_radius',
+                       'disk_ref_r_cylindrical']
 
     def __init__(self, grid, *, parameters=dict(), ensemble_size=None,
                  ensemble_seeds=None, dependencies={}, keep_galmag_field=False,
@@ -70,39 +76,31 @@ class GalMagDiskField(GalMagMagneticFieldBase):
                  disk_height_function=disk_prof.exponential_scale_height, # h(R)
                  disk_field_decay=True, disk_newman_boundary_condition_envelope=False,
                  ):
-        
+
         self._number_of_modes = number_of_modes
         self.galmag_generator_class = B_generator_disk
-        
-        super().__init__(grid=grid, parameters=parameters, 
+
+        super().__init__(grid=grid, parameters=parameters,
                          ensemble_size=ensemble_size,
-                         ensemble_seeds=ensemble_seeds, 
-                         dependencies=dependencies, 
+                         ensemble_seeds=ensemble_seeds,
+                         dependencies=dependencies,
                          keep_galmag_field=keep_galmag_field)
-    
+
         self._field_options = {'disk_shear_function': disk_shear_function,
                                'disk_rotation_function': disk_rotation_function,
                                'disk_height_function': disk_height_function,
                                'disk_field_decay': disk_field_decay,
-                               'disk_newman_boundary_condition_envelope': 
+                               'disk_newman_boundary_condition_envelope':
                                disk_newman_boundary_condition_envelope}
     @property
-    def field_checklist(self):
-        checklist = {'disk_modes_normalization': True,
-                     'disk_height': True,
-                     'disk_radius': True,
-                     'disk_turbulent_induction': True,
-                     'disk_dynamo_number': True,
-                     'disk_regularization_radius': True,
-                     'disk_ref_r_cylindrical': True}
-        
+    def parameter_names(self):
         # Includes individual parameters for each disk mode
-        checklist.update( {'mode_{0:d}'.format(i+1):True 
-                          for i in range(self._number_of_modes)} )
-        return checklist
-    
+        modes_list = ['mode_{0:d}'.format(i+1)
+                      for i in range(self._number_of_modes)]
+        return self.PARAMETER_NAMES + modes_list
+
     def compute_field(self, seed):
-        
+
         disk_mode_norm = []
         for i in range(self._number_of_modes):
             name = 'mode_{0:d}'.format(i+1)
@@ -111,15 +109,24 @@ class GalMagDiskField(GalMagMagneticFieldBase):
             else:
                 disk_mode_norm.append(0)
         self.parameters['disk_modes_normalization'] = np.array(disk_mode_norm)
-        
+
         return super().compute_field(seed)
-        
-            
+
+
 class GalMagHaloField(GalMagMagneticFieldBase):
     """
     An IMAGINE field constructed using GalMag's halo magnetic field
     """
-    NAME = 'galmag_halo_magnetic_field' 
+    NAME = 'galmag_halo_magnetic_field'
+
+    PARAMETER_NAMES = ['halo_turbulent_induction',
+                       'halo_rotation_induction',
+                       'halo_radius',
+                       'halo_ref_radius',
+                       'halo_ref_z',
+                       'halo_ref_Bphi',
+                       'halo_rotation_characteristic_radius',
+                       'halo_rotation_characteristic_height']
 
     def __init__(self, grid, *, parameters=dict(), ensemble_size=None,
                  ensemble_seeds=None, dependencies={}, keep_galmag_field=False,
@@ -131,14 +138,14 @@ class GalMagHaloField(GalMagMagneticFieldBase):
                  halo_compute_only_one_quadrant=True,
                  halo_growing_mode_only=False,
                  halo_Galerkin_ngrid=501):
-        
+
         self.galmag_generator_class = B_generator_halo
-        super().__init__(grid=grid, parameters=parameters, 
+        super().__init__(grid=grid, parameters=parameters,
                          ensemble_size=ensemble_size,
-                         ensemble_seeds=ensemble_seeds, 
-                         dependencies=dependencies, 
+                         ensemble_seeds=ensemble_seeds,
+                         dependencies=dependencies,
                          keep_galmag_field=keep_galmag_field)
-            
+
         self._field_options = {'halo_n_free_decay_modes': halo_n_free_decay_modes,
                                'halo_growing_mode_only': halo_growing_mode_only,
                                'halo_compute_only_one_quadrant': halo_compute_only_one_quadrant,
@@ -148,14 +155,3 @@ class GalMagHaloField(GalMagMagneticFieldBase):
                                'halo_rotation_function': halo_rotation_function,
                                'halo_alpha_function': halo_alpha_function}
 
-    @property
-    def field_checklist(self):
-        return {'halo_turbulent_induction': True,
-                'halo_rotation_induction': True,
-                'halo_radius': True,
-                'halo_ref_radius': True,
-                'halo_ref_z': True,
-                'halo_ref_Bphi': True,
-                'halo_rotation_characteristic_radius': True,
-                'halo_rotation_characteristic_height': True}
-    
